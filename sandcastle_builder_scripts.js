92f69638ecaa5fac47f104964fdd0f1f9f963170
brevehart <brevehart@users.noreply.github.com>
2015-06-05 23:03:14 -0700

    unknown minor changes

--- sandcastle_builder_scripts.js
+++ sandcastle_builder_scripts.js
@@ -9,3 +9,22 @@ var autoclicker = setInterval(function(){
 
 // some macro
 javascript:( function() { var js = document.createElement('script'); js.setAttribute('type', 'text/javascript'); js.setAttribute('src', 'http://xenko.github.io/BeachBall/BeachBall.js?'); document.head.appendChild(js); }() );
+
+
+BeachBall.ClickBeach = function (number) {
+	// added check to make best use of blast furnace
+	if(BeachBall.optimizeFurnaceSand 
+		&& Molpy.Boosts['Sand'].power > Molpy.Boosts['Castles'].nextCastleSand * .9 
+		&& BeachBall.Time_to_ONG < 300){
+			//console.log('too close to ONG');
+			return;  //try to leave as much sand as possible for blast furnace
+	}		
+	
+	if (Molpy.Got('Temporal Rift') == 0 && Molpy.ninjad != 0 && BeachBall.Time_to_ONG >= 5){
+		//console.log('clicking');
+		Molpy.ClickBeach();
+		
+	}
+};
+
+BeachBall.optimizeFurnaceSand = true;
